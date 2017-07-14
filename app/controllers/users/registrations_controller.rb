7dba5beafe9fcf91ae8503975f76ebc6f2ac48e5
Yoshihiro_Saito <yshr446@gmail.com>
2017-07-14 14:48:14 +0900

    bug fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string
     super
   end
+
+  def destroy
+    # ユーザーに紐付く投稿の削除
+    Photo.where(user_id:current_user.id).delete_all
+    super
+  end
+
 end
